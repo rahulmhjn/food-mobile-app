4612b14c09442e3879877ba18d0384d62d86779f
Rahul Mahajan <34473425+rahulmhjn@users.noreply.github.com>
2020-07-19 23:41:49 +0530

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
-        Authorization: 'Bearer WOQbTDNHBLjDhhz0tjgvARorvBVFjRByJKGyJKNaYd2vGTUE5QxwWRAmB5pbvRht3xTO91fGXx-dj6XNuBcrbVFhYWEU7yDee6dMUQ3gnAFY5EEaFjwerkvMHkrlXnYx'
+        Authorization: 'Bearer '
     }
 });
 
